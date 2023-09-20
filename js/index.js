ddc8897049dca60ce66a1f004bd02c00b164bdb1
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2023-09-20 20:20:48 -0300

    bug concertado

--- js/index.js
+++ js/index.js
@@ -11,21 +11,30 @@ function entrar() {
     const input3 = document.getElementById("isegundos");
     const segundos = Number(input3.value);
 
-    if(horas<=0, horas==''){
-        if(minutos<=0, minutos==''){
-           if(segundos<=0, segundos==''){
+    if(horas==''){
+        if(minutos==''){
+           if(segundos==''){
            alert("[Erro, Minuto Invalido]")
-       } else {
+       }else if(segundos<=0, segundos>60, segundos<=-1){
+        alert("[Erro, Minuto Invalido]")
+    } else {
            input.style.display = 'none'
            crono.style.display = 'flex'
        }
+    }else if(minutos<=0, minutos>60, minutos<=-1){
+        alert("[Erro, Minuto Invalido]")
     }else {
         input.style.display = 'none'
         crono.style.display = 'flex'
     }
-    }else {
+    }else if(horas<=0, horas>60, horas<=-1){
+        alert("[Erro, Minuto Invalido]")
+    } else{
         input.style.display = 'none'
         crono.style.display = 'flex'
     }
 
+    const horario1 = document.getElementById("horash");
+    const horario2 = document.getElementById("minutosh");
+    const horario3 = document.getElementById("segundosh");
 }
